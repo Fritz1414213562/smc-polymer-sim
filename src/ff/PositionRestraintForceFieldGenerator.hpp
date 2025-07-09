922c96a89c593c7928b085d949cb9568904a8a41
Fritz NAGAE <fritz1414213562@gmail.com>
2025-07-10 05:29:46 +0900

    feat: add 'PositionRestraint' external forcefield

---
+++ src/ff/PositionRestraintForceFieldGenerator.hpp
@@ -0,0 +1,43 @@
+#ifndef POLYMER_MDMC_POSITION_RESTRAINT_FORCE_FIELD_GENERATOR_HPP
+#define POLYMER_MDMC_POSITION_RESTRAINT_FORCE_FIELD_GENERATOR_HPP
+
+#include "ForceFieldGeneratorBase.hpp"
+#include "ForceFieldIDGenerator.hpp"
+
+#include <OpenMM.h>
+#include <fmt/core.h>
+
+#include <array>
+#include <memory>
+#include <string>
+#include <vector>
+
+class PositionRestraintForceFieldGenerator : public ForceFieldGeneratorBase
+{
+  public:
+    PositionRestraintForceFieldGenerator(
+        std::vector<std::size_t> indices, std::vector<std::array<double, 3>> positions,
+        std::vector<double>      ks,      std::vector<double>                v0s,
+        const bool use_periodic)
+        : indices_(indices), positions_(positions), ks_(ks), v0s_(v0s),
+          ffgen_id_(fmt::format("PR{}", ffid.gen()))
+    {
+        assert(this->indices_.size() == this->positions_.size());
+        assert(this->indices_.size() == this->ks_.size());
+        assert(this->indices_.size() == this->v0s_.size());
+    }
+
+    std::unique_ptr<OpenMM::Force> generate() const override;
+
+    std::string name() const override { return "PositionRestraint"; }
+
+  private:
+    std::vector<std::size_t>           indices_;
+    std::vector<std::array<double, 3>> positions_;
+    std::vector<double>                ks_;
+    std::vector<double>                v0s_;
+    bool                               use_periodic_;
+    std::string                        ffgen_id_;
+};
+
+#endif // POLYMER_MDMC_POSITION_RESTRAINT_FORCE_FIELD_GENERATOR_HPP
